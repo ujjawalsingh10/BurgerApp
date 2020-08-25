bb1ed2695fa7f5186457c9d317066cf224391ace
Ujjawal Singh <iamujjawal10.us@gmail.com>
2020-08-25 17:31:21 +0530

    Error Handling

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -102,7 +103,7 @@ class BurgerBuilder extends Component {
         }
         axios.post('/orders.json',order)
             .then(response => {
-                this.setState({ loading : false , purchasing : false });
+                this.setState({ loading : false , purchasing : false }); // This closes the modal
             })
             .catch(error => {
                 this.setState({ loading : false , purchasing : false });
@@ -143,4 +144,5 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+
+export default withErrorHandler(BurgerBuilder, axios);
