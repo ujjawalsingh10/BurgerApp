e5e4c00ab1b9d0f8cfed433621794fa85de3add1
Ujjawal Singh <iamujjawal10.us@gmail.com>
2020-11-08 23:57:22 +0530

    Getting a Token from the Backend

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -86,7 +86,7 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault(); 
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password);
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
     }
 
     render() {
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';   
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -21,7 +23,21 @@ export const authFail = (error) => {
 };
 
 export const auth = (email, password) => {
-     return disptach => {
-        disptach(authStart());   
+     return dispatch => {
+        dispatch(authStart()); 
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCdmRNhagiGewyBU0GbXybWcOvujx_cVp0',authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            }); 
      }
 }
